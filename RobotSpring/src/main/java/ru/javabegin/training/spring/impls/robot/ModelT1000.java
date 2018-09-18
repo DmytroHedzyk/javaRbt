dea98914ee6af62e18a16846de8d9cdb44d9f14f
Dmytro_Hedzyk <dmitro.rick16@gmail.com>
2018-09-18 18:23:13 +0300

    test6

--- RobotSpring/src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
+++ RobotSpring/src/main/java/ru/javabegin/training/spring/impls/robot/ModelT1000.java
@@ -11,6 +11,11 @@ public class ModelT1000 implements Robot {
 	private Leg leg;
 	private Head head;
 
+	private String color;
+	private int year;
+	private boolean soundEnabled;
+
+
 	public ModelT1000() {
 	}
 
@@ -21,11 +26,53 @@ public class ModelT1000 implements Robot {
 		this.head = head;
 	}
 
+	public ModelT1000(Hand hand, Leg leg, Head head, String color, int year, boolean soundEnabled) {
+		this.hand = hand;
+		this.leg = leg;
+		this.head = head;
+		this.color = color;
+		this.year = year;
+		this.soundEnabled = soundEnabled;
+	}
+
+    public ModelT1000(String color, int year, boolean soundEnabled) {
+        this.color = color;
+        this.year = year;
+        this.soundEnabled = soundEnabled;
+    }
+
+    public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public boolean isSoundEnabled() {
+		return soundEnabled;
+	}
+
+	public void setSoundEnabled(boolean soundEnabled) {
+		this.soundEnabled = soundEnabled;
+	}
+
 	@Override
 	public void action() {
 		head.calc();
 		hand.catchSomething();
 		leg.go();
+        System.out.println("color: " + color);
+        System.out.println("year: " + year);
+        System.out.println("can play sound: " + soundEnabled);
 	}
 
 	@Override
