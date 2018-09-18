530957c806693855d92bb0fab046cd6bcd329f7c
Dmytro_Hedzyk <dmitro.rick16@gmail.com>
2018-09-18 18:41:09 +0300

    test9

--- RobotSpring/src/main/java/ru/javabegin/training/spring/main/Start.java
+++ RobotSpring/src/main/java/ru/javabegin/training/spring/main/Start.java
@@ -10,5 +10,10 @@ public class Start {
 		ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
 		ModelT1000 t1000 = (ModelT1000) context.getBean("t1000");
 		t1000.action();
+
+		System.out.println();
+
+		ModelT1000 t1000Empty = (ModelT1000) context.getBean("t1000Empty");
+		t1000Empty.action();
 	}
 }
